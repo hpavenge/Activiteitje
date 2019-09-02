8fd0a5f372d10872c6f066dcc0e5ea93b665c83e
Nick van der Raaf <nickraaf@outlook.com>
2019-09-02 16:57:59 +0200

    Initial commit

---
+++ Activiteitje/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.cs" Inherits="Activiteitje.MvcApplication" Language="C#" %>
---
+++ Activiteitje/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Activiteitje")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("ede8ebfc-0486-42b1-9a5f-d03fb2777161")]
+
+[assembly: AssemblyProduct("Activiteitje")]
+[assembly: AssemblyCopyright("© 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("12.0.0.0")]
+[assembly: AssemblyFileVersion("12.0.7115.13953")]
+[assembly: AssemblyInformationalVersion("12.0.29")]
